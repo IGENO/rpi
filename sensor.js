1509f3af8705f31ae0ab83005e29f9226544eb79
Mark Clancy <mark@igeno.com>
2019-11-18 16:42:12 -0800

    working version mongodb find

--- sensor.js
+++ sensor.js
@@ -5,21 +5,21 @@ const MongoClient = require('mongodb').MongoClient
 const get = async () => {
 
 	let url = "mongodb+srv://vanmug:vanrasp8@cluster0-net0q.mongodb.net/test?retryWrites=true&w=majority"
-	let DATABASE = "vanmug"
+	let DATABASE = "meetup"
 	let COLLECTION = "readings"
 
 	let client = await MongoClient.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
-  let db = client.db(DATABASE)
+        let db = client.db(DATABASE)
 
-	let response = await connection.db.collection(COLLECTION).find({}).toArray()
+	let response = await db.collection(COLLECTION).findOne({})
 	console.log('response',response)
 
 	client.close()
 
 }
 
-let result = await get()
-
+get().then((result) => {
 console.log('result',result)
+})
 
 
