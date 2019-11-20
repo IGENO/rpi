526294002c1201549a071096b059b29ff2ba1e35
Mark Clancy <mark@igeno.com>
2019-11-20 00:58:50 +0000

    adapted to async

--- sensor2.js
+++ sensor2.js
@@ -6,10 +6,10 @@ const Sensor = require("node-dht-sensor").promises
 
 async function insertReading(reading) {
 	try {
-		let url = "mongodb+srv://vanmug:vanrasp8@cluster0-net0q.mongodb.net/test?retryWrites=true&w=majority"
+		let URL = "mongodb+srv://vanmug:vanrasp8@cluster0-net0q.mongodb.net/test?retryWrites=true&w=majority"
 		let DATABASE = "meetup"
 		let COLLECTION = "readings"
-		let client = await MongoClient.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
+		let client = await MongoClient.connect(URL, { useNewUrlParser: true, useUnifiedTopology: true })
 	  let db = client.db(DATABASE)
 		let response = await db.collection(COLLECTION).insertOne(reading)
 		client.close()
@@ -23,7 +23,7 @@ async function readSensor() {
 	try {
     let result = await Sensor.read(22, 4)
     console.log(
-      `temp: ${res.temperature.toFixed(1)}°C, humidity: ${res.humidity.toFixed(1)}%`
+      `temp: ${result.temperature.toFixed(1)}°C, humidity: ${result.humidity.toFixed(1)}%`
     )
     return result
 	} catch(error) {
@@ -35,7 +35,7 @@ async function handler() {
   try {
     let reading = await readSensor()
     console.log('reading=',reading)
-    let result = await insertReading({"hello":"world2"})
+    let result = await insertReading(reading)
 
   } catch (error) {
     console.error("error=", error)
